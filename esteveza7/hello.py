bb7989d1f05c97872251422aa9167d3a3fc1a2cd
AE1777 <323262519+AE1777@users.noreply.github.com>
2026-09-14 21:23:26 +0000

    Update hello Python examples

--- esteveza7/hello.py
+++ esteveza7/hello.py
@@ -1,2 +1,26 @@
-#!/usr/bin/env python3
 print("hello python!")
+
+print("My name is Timothy")
+
+print("My name is\nBenny")
+
+first = 4
+second = 5
+third = 6
+product = first * second * third
+print("The product of", first, second, "and", third, "is", product)
+
+first = 15
+second = 8
+product = first * second
+print(product)
+
+x = 10
+y = 346
+t = 23
+u = 45
+e = 34
+d = 12
+product = ((x * y)**u / (u + t) * (y - e)) * d
+print(product)
+
