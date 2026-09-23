440bd320bb3881d40c3e928b4e77f364eaee0bf0
AE1777 <323262519+AE1777@users.noreply.github.com>
2026-09-23 22:33:26 +0000

    Update chapter5_ex.py on esteveza7 file

--- esteveza7/chapter5_ex.py
+++ esteveza7/chapter5_ex.py
@@ -5,3 +5,19 @@ while total <= 25:
 5
 10
 20
+
+import random
+animals = ["goat", "cat", "lion", "pigeon", "ant", "giraffe", "snake"]
+for count in range(23):
+    number = random.randint(1, 99)
+    animal = random.choice(animals)
+    print(number, animal)
+
+
+
+temperatures = [30, 20, 2, -5, -15, -8, -1, 0, 5, 35]
+counter=0
+i=0
+for item in range(len(temperatures)):
+    temperatures.append(i)
+    print(len(temperatures))
